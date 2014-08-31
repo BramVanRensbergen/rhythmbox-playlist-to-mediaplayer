f8034bda94c5cb541d445f56dc8b6f278d9d18b3
Bram Van Rensbergen <mail@bramvanrensbergen.com>
2014-08-31 15:31:50 +0200

    Made it work.

--- rhythmbox-playlists-export.py
+++ rhythmbox-playlists-export.py
@@ -10,6 +10,7 @@ import glob
 import re
 import urllib
 import time
+import ntpath
 from xml.etree.ElementTree import Element, ElementTree
 import logging
 logging.basicConfig(level=logging.DEBUG) # filename='example.log',
@@ -20,27 +21,46 @@ logging.basicConfig(level=logging.DEBUG) # filename='example.log',
 logging.info("Beginning Sync")
 
 # Need to be configured
-local_username = 'decius'
-local_media = ["/media/media/Music"] #["/home/%s/%s" % (local_username, x) for x in ["Music", "Audiobooks", "Podcasts"]]
-local_playlists = '/home/decius/music_sync/temp' #'/tmp/rhythmbox_sync'
-remote_media = "/home/decius/music_sync/'"
-remote_playlists = '/home/decius/music_sync/playlists/'
+local_playlists = '/home/decius/Desktop/music/temp_playlists'  #playlists are temporarily stored here (permanently if KEEP_LOCAL_PLAYLIST_EXPORT = true)
+destination_media = "/home/decius/Desktop/music/"              #files are exported to this directory
+destination_playlists = '/home/decius/Desktop/music/Playlists' #playlists with new paths are stored here 
+skip_playlists = ['Recently Added', 'Recently Played', 'My Top Rated', 'check'] #Skip these playlists
+sync_playlists = ['altrock', 'chill'] #ONLY sync these playlists; if this is empty, sync ALL playlists except those in skip_playlists
 
-EXPORT_PLAYLISTS = True
+
 KEEP_LOCAL_PLAYLIST_EXPORT = True
 PLAYLIST_FORMAT = 'M3U' # only M3U currently supported, See note about Rhythmbox URI encoding above which also pertains to PLS support
-SYNC_PLAYLISTS = True
-DRY_RUN = False # Don't actually rsync anything
+rhythmbox_startup_wait = 1 #15 seconds, if Rhythmbox hasn't finished initializing the exports won't work (haven't found a programmatic way to check this)
 
-# Probably correct from above configuration
-local_media_bases = [x[:x.rfind('/')] for x in local_media]
-rhythmbox_startup_wait = 15 #15 seconds, if Rhythmbox hasn't finished initializing the exports won't work (haven't found a programmatic way to check this)
-rhythmbox_shutdown_wait = 3 # seconds
-skip_playlists = ['Recently Added', 'Recently Played', 'My Top Rated', 'check', 'cd']
 
-if not os.path.exists(local_playlists):
-  logging.info("Creating directory for local export")
-  os.makedirs(local_playlists)
+def rhythmbox_playlists_export():
+    #create folders if necessary
+    if not os.path.exists(destination_media):
+        logging.info("Creating directory for destination media")
+        os.makedirs(destination_media)
+    
+    if not os.path.exists(destination_playlists):
+        logging.info("Creating directory for destination playlists")
+        os.makedirs(destination_playlists)
+    
+    if not os.path.exists(local_playlists):
+        logging.info("Creating directory for local export")
+        os.makedirs(local_playlists)
+
+    #export rhythmbox playlists  
+    os.system('rhythmbox-client --no-present')
+    logging.info('Pausing %d seconds for Rhythmbox initialization' % (rhythmbox_startup_wait))
+    time.sleep(rhythmbox_startup_wait) # rhythmbox isn't ready until shortly after rhythmbox-client returns
+    export_playlists()
+
+    #copy all files in those playlists to destination dir, and export edited version of the playlists refering to the new source
+    sync_playlist_media()
+
+    #cleanup
+    if not KEEP_LOCAL_PLAYLIST_EXPORT:
+      logging.info("Removing folder used for local export")
+      os.system('rm -rf %s' % (local_playlists))
+  
 
 def export_playlists():
   logging.info("Exporting playlists...")
@@ -48,9 +68,18 @@ def export_playlists():
   sessionBus = dbus.SessionBus()
   playlistManager = sessionBus.get_object('org.gnome.Rhythmbox3', '/org/gnome/Rhythmbox3/PlaylistManager')
   asM3U = (PLAYLIST_FORMAT == 'M3U')
-  logging.debug("asM3U: %s" % (asM3U))
   for playlistName in playlistManager.GetPlaylists(dbus_interface='org.gnome.Rhythmbox3.PlaylistManager'):
-    if playlistName in skip_playlists: continue
+    
+    #check whether we have to sync this playlist!
+    syncThis = True
+    if len(sync_playlists) > 0 and not playlistName in sync_playlists: syncThis = False #working with whitelist, and this playlist is not on it
+    if playlistName in skip_playlists: syncThis = False #playlist is on blacklist, skip it
+    
+    if not syncThis:
+        logging.info('Skipping %s' %playlistName)
+        continue
+    
+    #Try to sync the playlist
     filename = "%s.%s" % (re.sub(clean_names_regex, '_', playlistName), PLAYLIST_FORMAT.lower())
     logging.info("Exporting '%s' to '%s'" % (playlistName, filename))
     try:
@@ -70,11 +99,8 @@ def export_playlists():
         break
 
 
-def sync_playlists():
-  logging.info("Syncing playlists...")
-  alterred_playlists = "%s/%s" % (local_playlists, "alterred")
-  if not os.path.exists(alterred_playlists):
-    os.makedirs(alterred_playlists)
+def sync_playlist_media():
+  logging.info("Syncing playlists and media...")
   for filename in glob.glob("%s/*.%s" % (local_playlists, PLAYLIST_FORMAT.lower())):
     playlist = open(filename, 'r')  
     playlist_text = playlist.readlines()
@@ -82,38 +108,27 @@ def sync_playlists():
     playlist_text_out = []
     for line in playlist_text:
       if line.startswith('#'):
-        playlist_text_out.append(line)
+        playlist_text_out.append(line) #copy comment lines as is
         continue
-      success = False
-      for media_loc in local_media_bases:
-        if line.find(media_loc) > -1:
-          playlist_text_out.append(line.replace(media_loc, remote_media))
-          cmd = 'cp -uv \"%s\" \"%s\"' % (line.rstrip('\n'), remote_media)
-         # logging.debug('Executing: %s' % (cmd))
-          os.system(cmd)
-          #logging.debug(line)
-          success = True
-          break
-      if not success:
-        logging.error("Couldn't determine how to modify file location for remote use: %s" % (line))
-    playlist_out = open("%s/%s" % (alterred_playlists, filename[filename.rfind('/')+1:]), 'w')
+      source_path = line.rstrip('\n')      
+      fname = path_leaf(source_path)
+      dest_path = destination_media + fname #path that file will have in destination
+      
+      #add converted path to new playlist
+      playlist_text_out.append(dest_path + '\n') 
+      
+      #copy file to destination!
+      cmd = 'cp -u \"%s\" \"%s\"' % (source_path, destination_media)
+      logging.info('Executing: %s' % (cmd))
+      os.system(cmd)
+
+    playlist_out = open("%s/%s" % (destination_playlists, filename[filename.rfind('/')+1:]), 'w')
     playlist_out.writelines(playlist_text_out)
     playlist_out.close()
-  if not DRY_RUN:
-    cmd = 'rsync -vrlptgz -e ssh "%s/"*.%s "%s@%s:%s" --delete-excluded' % (alterred_playlists, PLAYLIST_FORMAT.lower(), "ab", "cd", remote_playlists)
-    logging.debug('Executing: %s' % (cmd))
-    #os.system(cmd)
 
+#return the last part of a path (generally, the filename)
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
 
-if EXPORT_PLAYLISTS:
-  os.system('rhythmbox-client --no-present')
-  logging.info('Pausing %d seconds for Rhythmbox initialization' % (rhythmbox_startup_wait))
-  time.sleep(rhythmbox_startup_wait) # rhythmbox isn't ready until shortly after rhythmbox-client returns
-  export_playlists()
-
-if SYNC_PLAYLISTS:
-  sync_playlists()
-
-if not KEEP_LOCAL_PLAYLIST_EXPORT:
-  logging.info("Removing folder used for local export")
-  os.system('rm -rf %s' % (local_playlists))
+rhythmbox_playlists_export()
